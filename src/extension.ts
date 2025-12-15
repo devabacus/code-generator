7995ffac1a5a99a58f32ea52f439660328dd183b
devabacus <32705417+devabacus@users.noreply.github.com>
2025-12-15 16:38:18 +0500

    feat: Add initial extension activation and new commands for project generation, Python integration, data file creation, and K8s generation.

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,19 @@ import {
 } from "vscode";
 import { flutterHandler } from "./ui/flutter_menu";
 import { vsCodeExtHandler } from "./utils/vs_code_menu";
+import { createNewProject } from "./features/generation/commands/create_new_project";
+import { addPythonToProject } from "./features/generation/commands/add_python_to_project";
+import { createDataFilesByReplacement } from "./features/generation/commands/create_data_files_by_replacement";
+import { generateServerpodK8s } from "./features/generation/commands/generate_serverpod_k8s";
 
 export function activate(context: ExtensionContext) {
 	context.subscriptions.push(
 		commands.registerCommand("code-generator.flutter-handler", flutterHandler),
-		commands.registerCommand("code-generator.vsCodeExtHandler", vsCodeExtHandler),		
+		commands.registerCommand("code-generator.vsCodeExtHandler", vsCodeExtHandler),
+		commands.registerCommand("code-generator.createNewProject", createNewProject),
+		commands.registerCommand("code-generator.addPython", addPythonToProject),
+		commands.registerCommand("code-generator.createDataFiles", createDataFilesByReplacement),
+		commands.registerCommand("code-generator.generateK8s", generateServerpodK8s),
 	);
 }
 
